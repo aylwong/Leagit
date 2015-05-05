909ef6574c70406a1a9b826c54769a73100b2e46
Aylw <aylw@aylw-UL30A.(none)>
2015-05-05 13:41:19 -0400

    Committer: Aylw <aylw@aylw-UL30A.(none)>  Change list tournament format to table 	modified:   public/modules/competitors/config/competitors.client.config.js 	modified:   public/modules/tournaments/views/list.tournaments.client.view.html

--- public/modules/competitors/config/competitors.client.config.js
+++ public/modules/competitors/config/competitors.client.config.js
@@ -5,6 +5,5 @@ angular.module('competitors').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Competitors', 'competitors');
-		Menus.addMenuItem('topbar', 'New Competitor', 'competitors/create');
 	}
 ]);
