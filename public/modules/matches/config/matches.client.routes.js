3462960e06cce25bf2fc62c3a22ada0680123c16
Aylw <aylw@aylw-UL30A.(none)>
2015-05-05 13:27:51 -0400

    Committer: Aylw <aylw@aylw-UL30A.(none)>  Clean up some file names. 	deleted:    public/modules/competitors/views/create-competitor.client.view.html 	deleted:    public/modules/competitors/views/edit-competitor.client.view.html 	deleted:    public/modules/competitors/views/list-competitors.client.view.html 	deleted:    public/modules/competitors/views/search-competitors.client.view.html 	deleted:    public/modules/competitors/views/view-competitor.client.view.html 	modified:   public/modules/matches/config/matches.client.routes.js 	deleted:    public/modules/matches/views/create-match.client.view.html 	renamed:    public/modules/matches/views/edit-match.client.view.html -> public/modules/matches/views/edit.match.client.view.html 	renamed:    public/modules/matches/views/list-competitor-matches.client.view.html -> public/modules/matches/views/list.competitor.matches.client.view.html 	renamed:    public/modules/matches/views/list-matches.client.view.html -> public/modules/matches/views/list.matches.client.view.html 	modified:   public/modules/matches/views/mass.edit.matches.client.view.html 	renamed:    public/modules/matches/views/view-match.client.view.html -> public/modules/matches/views/view.match.client.view.html 	deleted:    public/modules/tournaments/views/create-tournament.client.view.html 	deleted:    public/modules/tournaments/views/edit-tournament.client.view.html 	deleted:    public/modules/tournaments/views/list-tournaments.client.view.html 	deleted:    public/modules/tournaments/views/view-match-tournament.client.view.html 	deleted:    public/modules/tournaments/views/view-tournament.client.view.html

--- public/modules/matches/config/matches.client.routes.js
+++ public/modules/matches/config/matches.client.routes.js
@@ -7,19 +7,19 @@ angular.module('matches').config(['$stateProvider',
 		$stateProvider.
 		state('listMatches', {
 			url: '/matches',
-			templateUrl: 'modules/matches/views/list-matches.client.view.html'
+			templateUrl: 'modules/matches/views/list.matches.client.view.html'
 		}).
 		state('listCompetitorMatches', {
 			url: '/competitors/:competitorId/matches',
-			templateUrl: 'modules/matches/views/list-competitor-matches.client.view.html'
+			templateUrl: 'modules/matches/views/list.competitor.matches.client.view.html'
 		}).
 		state('viewMatch', {
 			url: '/matches/:matchId',
-			templateUrl: 'modules/matches/views/view-match.client.view.html'
+			templateUrl: 'modules/matches/views/view.match.client.view.html'
 		}).
 		state('editMatch', {
 			url: '/matches/:matchId/edit',
-			templateUrl: 'modules/matches/views/edit-match.client.view.html'
+			templateUrl: 'modules/matches/views/edit.match.client.view.html'
 		})
 		.state('massEditMatch', {
 			url: '/tournaments/:tournamentId/matches/edit',
